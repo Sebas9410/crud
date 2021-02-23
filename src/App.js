cff272e2c9cf56f4189a2ca412033868d1d41ed9
Sebas9410 <53840547+Sebas9410@users.noreply.github.com>
2021-02-22 23:07:16 -0500

    video 15

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { isEmpty } from 'lodash'
+import shortid from 'shortid'
 
 
 function App() {
   const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
 
   const addTask = (e) =>{
     e.preventDefault()
@@ -12,7 +14,12 @@ function App() {
       return
     }
 
-    console.log("Ok")
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([ ...tasks, newTask])
     setTask("")
   }
 
@@ -24,11 +31,15 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la tarea</span>
-              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-              <button className="btn btn-warning btn-sm float-right">Editar</button>
-            </li>
+            {
+              tasks.map((task) => (
+              <li className="list-group-item" key={task.id}>
+                <span className="lead">{task.name}</span>
+                <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                <button className="btn btn-warning btn-sm float-right">Editar</button>
+              </li>
+              ))
+            }
           </ul>
         </div>
       
